590ff9c26e3970bb48d14c23fc7406b73860dcfc
Jay French <jay@frenchfryz.com>
2017-03-30 10:41:25 -0500

    Colonizer

--- src/game.data.js
+++ src/game.data.js
@@ -1,23 +1,23 @@
 let creepRoles = {
     'harvester': {
         optimalCount: 2,
-        body: [WORK,WORK,WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE,MOVE]
+        body: [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE]
     },
     'transporter': {
         optimalCount: 3,
-        body: [CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE]
+        body: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE]
     },
     'builder': {
         optimalCount: 3,
-        body: [WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE]
+        body: [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
     },
     'upgrader': {
         optimalCount: 2,
-        body: [WORK,WORK,WORK,WORK,WORK,CARRY,MOVE]
+        body: [WORK, WORK, WORK, WORK, WORK, CARRY, MOVE]
     },
     'scavenger': {
         optimalCount: 1,
-        body: [CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE]
+        body: [CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
     },
     'sentry': {
         optimalCount: 2,
@@ -27,26 +27,23 @@ let creepRoles = {
         optimalCount: 4,
         body: [TOUGH, TOUGH, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]
     },
-    'recycler': {
-        optimalCount: 0,
-        body: []
-    },
-    'claimer': {
-        optimalCount: 0,
-        body: []
-    }
+    'claimer': {optimalCount: 0, body: []},
+    'colonizer': {optimalCount: 0, body: []},
+    'recycler': {optimalCount: 0, body: []}
 };
 
 for (let roleName in creepRoles) {
-    creepRoles[roleName].bodyCost = _.sum(creepRoles[roleName].body.map(function(p) {return BODYPART_COST[p];}));
+    creepRoles[roleName].bodyCost = _.sum(creepRoles[roleName].body.map(function (p) {
+        return BODYPART_COST[p];
+    }));
 }
 
 let rooms = {
     'W84S61': {
         harvesterPosts: [{x: 30, y: 16}, {x: 13, y: 26}],
         scavengerPosts: [{x: 23, y: 21}, {x: 31, y: 10}],
-        sentryPosts: [{ x: 23, y: 19 }, { x: 28, y: 8 }],
-        soldierPosts: [{ x: 27, y: 12 }, { x: 28, y: 12 }, { x: 29, y: 12 }, { x: 30, y: 12 }, { x: 31, y: 12 }],
+        sentryPosts: [{x: 23, y: 19}, {x: 28, y: 8}],
+        soldierPosts: [{x: 27, y: 12}, {x: 28, y: 12}, {x: 29, y: 12}, {x: 30, y: 12}, {x: 31, y: 12}],
         upgraderPosts: [{x: 11, y: 27}, {x: 13, y: 27}/*, {x: 12, y: 27}, {x: 10, y: 27}*/],
         upgraderContainerPosition: {x: 12, y: 27}
     }
---
+++ src/role.colonizer.js
@@ -0,0 +1,76 @@
+/// <reference path="../scripts/_references.js" />
+
+function isColonizing(creep) {
+    if (!creep.memory.colonizing) {
+        let claimFlag = Game.flags['Claim'];
+        if (!claimFlag) return;
+
+        const rangeTo = creep.pos.getRangeTo(claimFlag);
+
+        if (rangeTo < 2) {
+            creep.memory.colonizing = true;
+        } else {
+            creep.say(rangeTo);
+            creep.moveTo(claimFlag, {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+    }
+
+    return creep.memory.colonizing;
+}
+
+module.exports = {
+    run: function (creep) {
+        if (!isColonizing(creep)) return;
+
+        let spawn = creep.room.pos.findClosestByRange(FIND_MY_SPAWNS);
+        let buildTarget = creep.room.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+
+        const harvesting = creep.memory.action === 'harvest';
+        const upgrading = creep.memory.action === 'upgrade';
+        const building = creep.memory.action === 'build';
+        const renewing = creep.memory.action === 'renew';
+
+        if (!renewing && spawn && !creep.spawning && creep.ticksToLive < 100) { // When spawning, apparently ticksToLive is 0
+            creep.memory.action = 'renew';
+            creep.say(creep.memory.action);
+        } else if ((upgrading || building) && creep.carry.energy === 0
+            || upgrading && buildTarget
+            || building && !buildTarget
+            || renewing && (creep.ticksToLive >= 1450 || creep.room.energyAvailable < 50)) {
+            creep.memory.action = 'harvest';
+            creep.say(creep.memory.action);
+        } else if (harvesting && creep.carry.energy === creep.carryCapacity) {
+            if (buildTarget) {
+                creep.memory.action = 'build';
+                creep.say(creep.memory.action);
+            } else {
+                creep.memory.action = 'upgrade';
+                creep.say(creep.memory.action);
+            }
+        }
+
+        if (harvesting) {
+            let source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+            if (source) {
+                if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        } else if (upgrading) {
+            if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        } else if (building) {
+            if (creep.build(buildTarget) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(buildTarget, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        } else if (renewing) {
+            creep.moveTo(spawn, {visualizePathStyle: {stroke: '#ffffff'}});
+            if (!creep.room.energyAvailable && creep.energy) {
+                creep.transfer(spawn, RESOURCE_ENERGY);
+            }
+        } else {
+            creep.say('help!');
+        }
+    }
+};
